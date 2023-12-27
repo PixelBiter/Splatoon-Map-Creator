3ea2e3293dc4e484edecf0c983e4a760f7cb933c
PixelToad <109037405+PixelBiter@users.noreply.github.com>
2023-12-27 19:49:53 +0000

    Replace Part A

--- Barnacle & Dime/G.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var ayer = 2.99
-var CamPosition
--- Barnacle & Dime/Main Scene.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-func _ready():
-	$Lights/Lights.visible = true
-	for i in range(10):
-		var Layer = load("res://layer.tscn")
-		var instance = Layer.instantiate()
-		add_child(instance)
-		G.ayer -= 0.6
